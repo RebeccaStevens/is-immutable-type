6a478f642de39e614772b97486269ac36fe56605
Rebecca Stevens <rebecca.stevens@outlook.co.nz>
2022-07-14 20:23:12 +1200

    test: add recursive type tests

---
+++ tests/recursive.test.ts
@@ -0,0 +1,55 @@
+import test from "ava";
+
+import { Immutableness } from "../src";
+
+import { runTestForGetTypeImmutableness } from "./helpers";
+
+test("simple", (t) => {
+  for (const code of [
+    "type Test<G> = Readonly<{ foo: Test<string> | string; }>;",
+    "type Test<G> = G extends string ? Readonly<{ foo: string }> : Test<string>",
+  ]) {
+    runTestForGetTypeImmutableness(
+      t,
+      code,
+      Immutableness.Immutable,
+      "handles recursive immutable types"
+    );
+  }
+
+  for (const code of [
+    "type Test<G> = Readonly<{ foo: ReadonlyArray<Test<string>> | string; }>;",
+    "type Test<G> = G extends string ? ReadonlyArray<string> : Test<string>",
+  ]) {
+    runTestForGetTypeImmutableness(
+      t,
+      code,
+      Immutableness.ReadonlyDeep,
+      "handles recursive deeply readonly types"
+    );
+  }
+
+  for (const code of [
+    "type Test<G> = Readonly<{ foo: Array<Test<string>> | string; }>;",
+    "type Test<G> = G extends string ? Readonly<{ foo: Array<string>; }> : Test<string>",
+  ]) {
+    runTestForGetTypeImmutableness(
+      t,
+      code,
+      Immutableness.ReadonlyShallow,
+      "handles recursive shallowly readonly types"
+    );
+  }
+
+  for (const code of [
+    "type Test<G> = { foo: Test<string> | string; };",
+    "type Test<G> = G extends string ? { foo: string } : Test<string>",
+  ]) {
+    runTestForGetTypeImmutableness(
+      t,
+      code,
+      Immutableness.Mutable,
+      "handles recursive mutable types"
+    );
+  }
+});
