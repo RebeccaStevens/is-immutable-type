c0710e3b30db758dbd6af6d4d85a57c549b79c1f
Rebecca Stevens <rebecca.stevens@outlook.co.nz>
2023-02-05 10:33:56 +1300

    test: test destructuring of arrays and objects
    close #48

---
+++ tests/destruct.test.ts
@@ -0,0 +1,55 @@
+import test from "ava";
+
+import { Immutability } from "../src";
+
+import { runTestImmutability } from "./helpers";
+
+test("destructuring arrays", (t) => {
+  for (const code of [
+    "type Test = typeof a; const [a, b, ...c] = [1, 2, 3, 4];",
+    "type Test = typeof b; const [a, b, ...c] = [1, 2, 3, 4];",
+  ]) {
+    runTestImmutability(
+      t,
+      code,
+      Immutability.Immutable,
+      "immutable elements in mutable destructed array are detected as immutable"
+    );
+  }
+
+  for (const code of [
+    "type Test = typeof c; const [a, b, ...c] = [1, 2, 3, 4];",
+  ]) {
+    runTestImmutability(
+      t,
+      code,
+      Immutability.Mutable,
+      "rest property is detected as a mutable array"
+    );
+  }
+});
+
+test("destructuring objects", (t) => {
+  for (const code of [
+    "type Test = typeof a; const { a, b, ...c } = { a: 1, b: 2, d: 4, e: 5 };",
+    "type Test = typeof b; const { a, b, ...c } = { a: 1, b: 2, d: 4, e: 5 };",
+  ]) {
+    runTestImmutability(
+      t,
+      code,
+      Immutability.Immutable,
+      "immutable elements in mutable destructed object are detected as immutable"
+    );
+  }
+
+  for (const code of [
+    "type Test = typeof c; const { a, b, ...c } = { a: 1, b: 2, d: 4, e: 5 };",
+  ]) {
+    runTestImmutability(
+      t,
+      code,
+      Immutability.Mutable,
+      "rest property is detected as a mutable array"
+    );
+  }
+});
